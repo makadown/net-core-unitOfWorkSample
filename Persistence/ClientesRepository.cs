5e5df5b1d90663a0b27a75c16d83fd1efb795745
Makadown <maseflores79@gmail.com>
2019-04-30 06:13:02 -0500

    Ya jala el getall

--- Persistence/ClientesRepository.cs
+++ Persistence/ClientesRepository.cs
@@ -5,7 +5,7 @@ namespace unitOfWorkSample.Persistence
 {
     public class ClienteRepository : GenericRepository<Clientes>, IClientesRepository
     {
-        private readonly MySqlContext _context;
+        // private readonly MySqlContext _context;
         public ClienteRepository(MySqlContext context)
         {
             _context = context;
